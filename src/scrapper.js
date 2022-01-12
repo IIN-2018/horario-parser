4b28d0c305d0e4717c62fb87e50194725d75c13a
Sebastian Ramirez <ramirezmatias946@gmail.com>
2022-01-11 22:45:46 -0300

    Agregamos el reject unauthorized para realizar la descarga del excel.

--- src/scrapper.js
+++ src/scrapper.js
@@ -6,7 +6,8 @@ module.exports = async (url) => {
         let HTML = await requestPromise(url, {
             headers: {
                 'User-Agent': ' Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
-            }
+            },
+            "rejectUnauthorized": false,
         });
 
         const $ = cheerio.load(HTML);
