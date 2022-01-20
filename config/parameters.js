af101ccb77da6d2661100f9caf0a5aaefe4f79bb
Matias Sebastian Ramirez Brizuela <ramirezmatias946@gmail.com>
2022-01-19 22:24:13 -0300

    Se agrega a la configuracion los sheets y carreras

--- config/parameters.js
+++ config/parameters.js
@@ -1,10 +1,38 @@
 module.exports = {
     url: 'https://www.pol.una.py/academico/horarios-de-clases-y-examenes/',
     pathname: './public/horario.xlsx',
-    sheets: {
-        IAE: 'IAE',
-        IIN: 'IIN',
-        IEK: 'IEK',
-        IEL: 'IEK'
+    sheets: [
+        "IAE",
+        "ICM",
+        "IEK",
+        "IEL",
+        "IEN",
+        "IIN",
+        "IMK",
+        "ISP",
+        "LCA",
+        "LCI",
+        "LCIk",
+        "LEL",
+        "LGH",
+        "TSE"
+    ],
+    carreras:
+    {
+        IAE: 'Ingenieria Aeronautica',
+        ICM: 'Ingenieria en Ciencias de los Materiales',
+        IEK: 'Ingenieria en Electronica',
+        IEL: 'Ingenieria en Electricidad',
+        IEN: 'Ingenieria en Energia',
+        IIN: 'Ingenieria en Informatica',
+        IMK: 'Ingenieria en Marketing',
+        ISP: 'Ingenieria en Sistemas de Produccion',
+        LCA: 'Licenciatura en Ciencias Atmosferica',
+        LCI: 'Licenciatura en Ciencias de la Informacion',
+        LCIk: 'Licenciatura en Ciencias Informaticas',
+        LEL: 'Licenciatura en Electricidad',
+        LGH: 'Licenciatura en Gestion de la Hospitalidad',
+        TSE: 'Tecnico Superior en Electronica'
     }
+
 };
