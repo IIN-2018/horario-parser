bb60104eb03620c6bf595629af30c45d8a8540d0
Matias Sebastian Ramirez Brizuela <ramirezmatias946@gmail.com>
2022-01-25 22:23:31 -0300

    Activamos los tests

--- src/tests/download.test.js
+++ src/tests/download.test.js
@@ -1,7 +1,7 @@
 const { downloadHorarioExcel } = require("../api/download");
 const fs = require("fs");
 
-describe.skip('Descargar el Excel del Link de Descarga de la Pagina de la Poli', () => {
+describe('Descargar el Excel del Link de Descarga de la Pagina de la Poli', () => {
     test('Descargar el Excel en la carpeta public', async () => {
         let url = await downloadHorarioExcel("https://www.pol.una.py/academico/horarios-de-clases-y-examenes/", "./public/horario.xlsx");
         let excelExists = fs.existsSync("./public/horario.xlsx");
