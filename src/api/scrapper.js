227f3c256ce892c8a679ca47c248524ac8c510cb
Matias Sebastian Ramirez Brizuela <ramirezmatias946@gmail.com>
2022-08-06 21:38:57 -0400

    Optimizacion del codigo del scrapper
    Co-authored-by: ricardogaona <ricardogaona@users.noreply.github.com>

--- src/api/scrapper.js
+++ src/api/scrapper.js
@@ -1,6 +1,7 @@
 const cheerio = require('cheerio');
 const { default: axios } = require('axios');
 const https = require('https');
+const { htmlTest } = require('../utils/test_html');
 
 /**
  * 
@@ -8,20 +9,23 @@ const https = require('https');
  * @returns Retorna una promesa que al finalizar correctamente retorna el html de la pagina de la Poli
  */
 const getHtmlPoli = async (url) => {
-    try {
-        const options = {
-            headers: {
-                'User-Agent': ' Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
-            },
-            httpsAgent: new https.Agent({
-                rejectUnauthorized: false
-            })
-        };
-        const html = await axios(url, options);
-        return cheerio.load(html.data);
-    } catch (error) {
-        throw new Error(`Error al obtener el html de la pagina: ${url}\n${error}`);
-    }
+  try {
+    const options = {
+      headers: {
+        'User-Agent': ' Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+      },
+      httpsAgent: new https.Agent({
+        rejectUnauthorized: false
+      })
+    };
+    // return cheerio.load(htmlTest);
+
+    const html = await axios(url, options);
+    return cheerio.load(html.data);
+
+  } catch (error) {
+    throw new Error(`Error al obtener el html de la pagina: ${url}\n${error}`);
+  }
 }
 
 /**
@@ -30,41 +34,37 @@ const getHtmlPoli = async (url) => {
  * @returns Retorna el URL de descarga del Horario de Clases
  */
 const getUrlDescargaHorarioClases = async ($) => {
-    try {
+  try {
 
-        //Seccion de html en la que se encuentra el horario
-        const horarioSecciones = $('.elementor.elementor-1100')
-            .children()
-            .children()
-            .last();
+    //Seccion de html en la que se encuentra el horario
+    const horarioSeccion = $('.elementor.elementor-1100')
+      .children()
+      .children()
+      .children()
+      .last();
 
-        //Seccion de html en la que se encuentra el titulo del horario
-        const tituloSeccionHorarioPlaificacion = horarioSecciones
-            .children()
-            .children()
-            .children()
-            .children()
-            .children()
-            .text()
-            .trim()
-            .replace('Grado', 'Grado - ');
+    //Seccion de html en la que se encuentra el titulo del horario
+    const tituloSeccionHorarioPlaificacion = horarioSeccion
+      .find('h3')
+      .first()
+      .text()
+      .trim();
 
-        //Seccion de html en la que se encuentra el enlace de descarga del horario
-        const urlHorarioDescarga = horarioSecciones
-            .children()
-            .children()
-            .children()
-            .children()
-            .children()
-            .children('p')
-            .children()
-            .attr('href');
+    //Seccion de html en la que se encuentra el enlace de descarga del horario
+    const urlHorarioDescarga = horarioSeccion
+      .find('p')
+      .children()
+      .attr('href');
 
-        console.log(tituloSeccionHorarioPlaificacion, '\n', 'Se ha Conseguido exitosamente el enlace de Descarga: ', urlHorarioDescarga);
-        return urlHorarioDescarga;
-    } catch (error) {
-        throw new Error(`Error al obtener el enlace de descarga del horario de clases: ${error}`);
+    if (!urlHorarioDescarga) {
+      throw new Error(`Error al obtener el URL realizando el Web Scrapping`);
     }
+
+    console.log(tituloSeccionHorarioPlaificacion, '\n', 'Se ha Conseguido exitosamente el enlace de Descarga: ', urlHorarioDescarga);
+    return urlHorarioDescarga;
+  } catch (error) {
+    throw new Error(`Error al obtener el enlace de descarga del horario de clases: ${error}`);
+  }
 }
 
 /**
@@ -73,16 +73,16 @@ const getUrlDescargaHorarioClases = async ($) => {
  * @returns Retorna una promesa que al finalizar correctamente retorna el enlace de descarga del Horario de Clases
  */
 const scrapper = async (url) => {
-    try {
-        const $ = await getHtmlPoli(url);
-        const urlHorarioDescarga = await getUrlDescargaHorarioClases($);
-        return urlHorarioDescarga;
-    } catch (err) {
-        console.log(err);
-        throw err;
-    }
+  try {
+    const $ = await getHtmlPoli(url);
+    const urlHorarioDescarga = await getUrlDescargaHorarioClases($);
+    return urlHorarioDescarga;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
 }
 
 module.exports = {
-    scrapper
+  scrapper
 }
