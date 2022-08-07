be8f314249fcf4da8653a6bbdeb7065f535c85e6
Matias Sebastian Ramirez Brizuela <ramirezmatias946@gmail.com>
2022-08-06 21:33:51 -0400

    Creamos un archivo JSON de los datos antes de retornarlo.
    Co-authored-by: ricardogaona <ricardogaona@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,30 +1,41 @@
 const { downloadHorarioExcel } = require('./api/download.js');
 const { scrapper } = require('./api/scrapper.js');
 const { parseExcel } = require('./api/excel2json.js');
+const fs = require('fs');
 const parameters = require('../config/parameters.js');
 
 const completeScrapperParser = async () => {
-    return scrapper(parameters.url)
-        .then(link => {
-            return downloadHorarioExcel(link, parameters.pathname)
-                .then(async () => {
-                    const { carreras, horarios } = await parseExcel();
-                    return {
-                        carreras,
-                        horarios
-                    }
-                })
-                .catch(err => {
-                    console.log('Download Error: ', err);
-                    return null;
-                });
-        }).catch(err => {
-            console.log('Scraping Error: ', err);
-            return null;
+  return scrapper(parameters.url)
+    .then(link => {
+      return downloadHorarioExcel(link, parameters.pathname)
+        .then(async () => {
+          const { carreras, horarios } = await parseExcel();
+          const myHorario =
+          {
+            carreras,
+            horarios
+          };
+          createHorarioFile(myHorario);
+          return myHorario;
+        })
+        .catch(err => {
+          console.log('Download Error: ', err);
+          return null;
         });
+    }).catch(err => {
+      console.log('Scraping Error: ', err);
+      return null;
+    });
 }
 
+//scrapper(parameters.url).then(link => console.log(`Enlace obtenido: ${link}`));
+completeScrapperParser();
+
 module.exports = {
-    completeScrapperParser,
-    scrapper,
+  completeScrapperParser,
+  scrapper,
 }
+function createHorarioFile(myHorario) {
+  fs.writeFileSync('horario.json', JSON.stringify(myHorario));
+}
+
